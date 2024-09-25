24e53b56ddd54030da5f8f0bd4a915e8bd553df0
pattriciaruth <p.das.11@student.scu.edu.au>
2024-09-25 18:14:12 +0800

    Update crowdfunding_db.js

--- crowdfunding_db.js
+++ crowdfunding_db.js
@@ -3,7 +3,7 @@ const mysql = require('mysql');
 const db = mysql.createConnection({
     host: 'localhost:3306',
     user: 'root',  // Use your MySQL username
-    password: '#Beyourself08',  // Use your MySQL password
+    password: '******',  // Use your MySQL password
     database: 'crowdfunding_db'
 });
 
